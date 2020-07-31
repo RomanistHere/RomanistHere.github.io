09299c046a98b62612d17a5095522a64ccaeb2ba
unknown <romanisthere@gmail.com>
2020-07-31 18:07:45 +0300

    change preprender urls

--- prerender-urls.js
+++ prerender-urls.js
@@ -1,5 +1,2 @@
-require("@babel/register")({
-    presets: ["@babel/preset-env", "@babel/preset-react"]
-});
-
-module.exports = require('./prerender-urls-es6.mjs');
+require = require('esm')(module)
+module.exports = require('./prerender-urls.esm.js')
--- src/static/functions.js
+++ src/static/functions.js
@@ -62,21 +62,21 @@ const useTimeout = (callback, delay) => {
 }
 
 const useViewport = () => {
-	const [width, setWidth] = useState(window ? window.innerWidth : 1920)
-	// Add a second state variable "height" and default it to the current window height
-	const [height, setHeight] = useState(window ? window.innerHeight : 900)
+	if (typeof window === 'undefined')
+		return { width: 1366, height: 600 }
+
+	const [width, setWidth] = useState(window.innerWidth)
+	const [height, setHeight] = useState(window.innerHeight)
 
 	useEffect(() => {
-		if (typeof window !== 'undefined') {
-			const handleWindowResize = () => {
-				setWidth(window.innerWidth)
-				// Set the height in state as well as the width
-				setHeight(window.innerHeight)
-			}
-
-			window.addEventListener("resize", handleWindowResize)
-			return () => window.removeEventListener("resize", handleWindowResize)
+		const handleWindowResize = () => {
+			setWidth(window.innerWidth)
+			// Set the height in state as well as the width
+			setHeight(window.innerHeight)
 		}
+
+		window.addEventListener("resize", handleWindowResize)
+		return () => window.removeEventListener("resize", handleWindowResize)
 	}, [])
 
 	// Return both the height and width
