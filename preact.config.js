3a08995fdf2fce84c8077ad713dcf1cdae1e4aae
unknown <romanisthere@gmail.com>
2020-08-03 17:21:41 +0300

    fix config

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,11 @@
-export default (config, env, helpers) => {
+export default (config, env, helpers, options) => {
     const css = helpers.getLoadersByName(config, 'css-loader')[0];
     css.loader.options.modules = false;
+
+    const rule = {
+        test: /\.(otf|pdf)$/,
+        loader: 'file-loader'
+    }
+	// add a new loader
+	config.module.rules.push(rule);
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './media/fonts/handFont.ttf'
 import './media/fonts/handFont2.ttf'
-// import './media/fonts/handFont3.otf'
+import './media/fonts/handFont3.otf'
 
 import './static/variables.css'
 import './index.css'
 
 // comment on build
-import "preact/debug"
+// import "preact/debug"
 
 import App from './pages/App'
 
