34c2874cfcbf52aa892faf809071ec1ac42026be
unknown <romanisthere@gmail.com>
2020-08-04 17:03:47 +0300

    try sourcemap

--- preact.config.js
+++ preact.config.js
@@ -2,6 +2,9 @@ export default (config, env, helpers, options) => {
     const css = helpers.getLoadersByName(config, 'css-loader')[0];
     css.loader.options.modules = false;
 
+    const sass = helpers.getLoadersByName(config, 'proxy-loader')[1];
+    sass.loader.options.sourceMap = true;
+
     const rule = {
         test: /\.(otf|pdf)$/,
         loader: 'file-loader'
