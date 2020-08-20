cc9e1c5fbfec42c1618d5a8a498312c547f32929
unknown <romanisthere@gmail.com>
2020-08-20 13:54:30 +0300

    add mechanics to magic page

--- src/pages/Magic/Magic.js
+++ src/pages/Magic/Magic.js
@@ -4,8 +4,71 @@ import Typing from '../../components/Typing/Typing'
 import LinkBack from '../../components/LinkBack/LinkBack'
 
 import content from '../../static/content'
+import manage from '../../static/manage'
+import { secondsToHms } from '../../static/functions'
 
 import './Magic.css'
+
+const setText = (ratio, key) => {
+    const text = {
+        less: `the size of`,
+        equal: `is almost like`,
+        more: `times bigger then the size of`,
+    }
+
+    return key === 'equal' ? text[key] : `${ratio.toFixed(1)} ${text[key]}`
+}
+
+const getText = (val, size) => {
+    const ratio = val/size
+    const equality =
+        ((ratio <= 1.1) && (ratio >= .9)) ? 'equal' :
+        ((ratio < .9) && (ratio >= .4)) ? 'less' :
+        ((ratio > 1.1) && (ratio <= 1.6)) ? 'more' :
+        (ratio <= .3) ? 'less' :
+        null
+    const caption = equality ? setText(ratio, equality) : false
+
+    return caption
+}
+
+const getArea = (val, numb = 1) => {
+    const { areaData } = manage
+    try {
+        const { size, value } = areaData[numb]
+        const text = getText(val, size)
+        const fullText = text ? `${text} ${value}` : getArea(val, numb + 1)
+        return fullText
+    } catch (e) {
+        return `It's almost nothing`
+    }
+}
+
+const MagicContent = ({ cleanedArea, numbOfItems }) => {
+    // cleanedArea = .5
+    const time = secondsToHms(cleanedArea * 1.32)
+    const averageScreenArea = 0.12
+    const areaInMetres = cleanedArea * averageScreenArea
+    const sqMetresToFt = 10.764
+    const areaInFt = areaInMetres * sqMetresToFt
+    return <>
+        <p className="magic__text">
+            Total area of removed overlays is about {cleanedArea.toFixed(1)} of your screen sizes
+        </p>
+        <p className="magic__text">
+            And total number of this popups, overlays and other elements is {numbOfItems}
+        </p>
+        <p className="magic__text">
+            Saved time: {time ? time : '0 seconds'}
+        </p>
+        <p className="magic__text">
+            Area in square metres: {areaInMetres.toFixed(1)} or {areaInFt.toFixed(1)} square feet
+        </p>
+        <p className="magic__text">
+            {getArea(areaInMetres)}
+        </p>
+    </>
+}
 
 const Magic = () => {
     const [isAllowed, allow] = useState(false)
@@ -17,7 +80,8 @@ const Magic = () => {
     useEffect(() => {
         if (typeof window !== 'undefined') {
             document.title = 'Watch some magic'
-
+            // connect to PopUpOFF
+            document.dispatchEvent(new CustomEvent('showPopUpOFFStats', { detail: 'letTheShowBegin' }))
             document.addEventListener('PopUpOFFStats', ({ detail }) => {
                 setStats(detail)
                 allow(true)
@@ -25,20 +89,14 @@ const Magic = () => {
         }
     }, [])
 
-  	return 	isAllowed ?
-            <section className="main magic">
-                <p className="magic__text">
-                    Total area of removed overlays is about {stats.cleanedArea.toFixed(1)} of your screen sizes
-                </p>
-                <p className="magic__text">
-                    And total number of this popups, overlays and other elements is {stats.numbOfItems}
-                </p>
-                <LinkBack className="no_page__back" />
-            </section> :
-            <section className="main magic">
-                <span className="main__no_acess">
-                    You do not have acess to the magic... yet
-                </span>
+  	return 	<section className="main magic">
+                {
+                    isAllowed ?
+                    MagicContent(stats) :
+                    <span className="main__no_acess">
+                        You do not have acess to the magic... yet
+                    </span>
+                }
                 <LinkBack className="no_page__back" />
             </section>
 }
--- src/static/manage.js
+++ src/static/manage.js
@@ -1,21 +1,76 @@
 const manage = {
-	'offer_switcher__hide_time': 15000,
-	'offer_switcher__show_time': 2000,
-	'card_fill__delay': 1700,
-	'form__typing_1__show_time': 2000,
-	'form__typing_2__show_time': 0,
-	'form__typing_3__show_time': 0,
-	'form__typing_5__hide_time': 10000,
-	'forbidShow_time': 5000,
-	'card__typing__show_time': 10000,
-	'card__typing__hide_time': 30000,
-	'card__typings__show_time': 12000,
-	'card__typings__hide_time': 9000,
-	'apps__typing': 2000,
+	offer_switcher__hide_time: 15000,
+	offer_switcher__show_time: 2000,
+	card_fill__delay: 1700,
+	form__typing_1__show_time: 2000,
+	form__typing_2__show_time: 0,
+	form__typing_3__show_time: 0,
+	form__typing_5__hide_time: 10000,
+	forbidShow_time: 5000,
+	card__typing__show_time: 10000,
+	card__typing__hide_time: 30000,
+	card__typings__show_time: 12000,
+	card__typings__hide_time: 9000,
+	apps__typing: 2000,
 	browsers: {
 		1: [0, 1, 2, 3],
 		2: [0, 1],
 		3: [0, 1],
+	},
+	// square metres
+	areaData: {
+	    1: {
+	        size: 0.01,
+	        value: `iPhone 11 Pro`
+	    },
+	    2: {
+	        size: 0.04,
+	        value: `iPad Pro`
+	    },
+	    3: {
+	        size: 0.18,
+	        value: `small bathroom mirror`
+	    },
+	    4: {
+	        size: 0.41,
+	        value: `the Mona Lisa`
+	    },
+	    5: {
+	        size: 2.6,
+	        value: `an average double bed`
+	    },
+	    6: {
+	        size: 11.5,
+	        value: `an average parking space`
+	    },
+	    7: {
+	        size: 40,
+	        value: `the largest book in the world`
+	    },
+	    8: {
+	        size: 67,
+	        value: `the largest painting at the Louvre Museum, "The Wedding Feast at Cana"`
+	    },
+	    9: {
+	        size: 125,
+	        value: `Boeing 737 wing area`
+	    },
+	    10: {
+	        size: 307,
+	        value: `world's smallest inhabited island`
+	    },
+	    11: {
+	        size: 1250,
+	        value: `olimpic pool`
+	    },
+	    12: {
+	        size: 10625,
+	        value: `standart soccer field`
+	    },
+	    13: {
+	        size: 490000,
+	        value: `Vatican City`
+	    },
 	}
 }
 
