dfb7eff6cbb570233526051fa03f43b20651ccff
unknown <romanisthere@gmail.com>
2020-08-21 16:13:35 +0300

    Updates

--- functions.js
+++ functions.js
@@ -99,6 +99,19 @@ const importAll = (r) => {
     return images
 }
 
+const secondsToHms = (d) => {
+    d = Number(d)
+    const h = Math.floor(d / 3600)
+    const m = Math.floor(d % 3600 / 60)
+    const s = Math.floor(d % 3600 % 60)
+
+    const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : ""
+    const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : ""
+    const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : ""
+
+    return hDisplay + mDisplay + sDisplay
+}
+
 export {
 	querySelector,
 	addClass,
@@ -114,4 +127,5 @@ export {
 	findPostBySlug,
 	importAll,
 	isFocused,
+	secondsToHms,
 }
--- posts.js
+++ posts.js
@@ -7,7 +7,7 @@ const posts = [
 	// 	image: ``,
 	// 	content: ``
 	// }
-	//<p class="article__subtitle part1"></p>
+	//<h3 class="article__subtitle part1"></h3>
 	//<p class="article__text"></p>
 	//<a href="link" rel="noreferrer" target="_blank" class="link"></a>
 	{
@@ -17,15 +17,15 @@ const posts = [
 		desc: `My thoughts on how to choose software, make impact on the world and what am I doing here.`,
 		image: `presentation.png`,
 		content: `
-			<p class="article__subtitle part1">Part I</p>
+			<h3 class="article__subtitle part1">Part I</h3>
 
 			<p class="article__text">There are <span class="article__col-1">good</span> products and <span class="article__col-2">bad</span> ones. I’ll explain by example. Imagine the situation: You are using an <span class="article__strong">ad blocker/messenger app</span>. You’re using it because the day you installed <span class="article__strong">it was most popular/everyone used it</span>. You're enjoying it and maybe even paying for it. But one day you see <span class="article__strong">ads on a popular website/news that the company listens to and reads everything you say through your messenger app</span> and you catch a glimpse: “Wow, that’s inappropriate. I have to deal with it” - And then… You just don’t. <span class="article__strong">There are no websites with ads other day/company that owns mess app publicly apologizes and assures that this will not happen again</span>. Everything is resolved "by itself" now. You forget about it till the next time something will happen. And it certainly will. Because it's an example of a <span class="article__col-2">bad</span> product. An inappropriate behaviour here is <span class="article__strong">betrayal of the idea for profit/invasion of privacy</span>. Not only are you not fighting it, but also letting it circulate. If someone asked your advice about <span class="article__strong">an ad blocker/a mess app</span>, you would name the <span class="article__col-2">bad</span> one. This way you're making <span class="article__col-2">bad</span> products spread and letting everyone know that you are good with such an attitude towards you. So they build more apps and services like this. Sorry, but you’re making the internet worse.</p>
 
-			<p class="article__subtitle part2">Part II</p>
+			<h3 class="article__subtitle part2">Part II</h3>
 
 			<p class="article__text">So… What can you do? Almost any product has its <span class="article__col-1">good</span> analogs. You can find it if you spend a few more minutes before installing. But your friends(if we're talking about messenger, for example) are not going to be there, aren’t they? Changing the world isn’t our goal - changing ourselves is. If one does want to chat with you, one is going to find a way to do it. You would be surprised how much you can change outside yourself once you did it inside. Explain your position to your friends. Tell them you want to use <span class="article__col-1">good</span> software and not <span class="article__col-2">bad</span> one and why. By the way, why would you want it?</p>
 
-			<p class="article__subtitle part3">Part III</p>
+			<h3 class="article__subtitle part3">Part III</h3>
 
 			<p class="article__text">As I wrote above, by not fighting inappropriate behaviour you let it continue. You make the internet deteriorate. Let the guys who treat you like that see that you are ok with it, so they continue to treat you like this. So, once they can gain profit from doing so, they won't hesitate - they already succeeded the last time. It’s a sort of a Vicious Cycle. You can’t sit, do nothing and expect that the world is going to treat you good just because it's you. Would you like to break the Vicious Cycle? What can you gain from it?</p>
 
@@ -37,7 +37,7 @@ const posts = [
 			</ul>
 			<p class="article__text">If everyone had behaved themself like this, there wouldn’t have been <span class="article__col-2">bad</span> products across the world. And I'm not talking about software now. But since our topic is, there is a list of some nice(not) treatment you could have missed:</p>
 
-			<p class="article__subtitle part4">Part IV</p>
+			<h3 class="article__subtitle part4">Part IV</h3>
 
 			<p class="article__text">Below are only the facts, confirmed and apologized by companies:</p>
 			<ul class="article__list article__text">
@@ -169,7 +169,7 @@ const posts = [
 
 			<p class="article__text">It must be noted that it's possible for commercial and proprietary(closed-source) software to be the <span class="article__col-1">good</span> one but it's rather rare.</p>
 
-			<p class="article__subtitle part5">Part V</p>
+			<h3 class="article__subtitle part5">Part V</h3>
 
 			<p class="article__text">Finally, what my mission is. As a  human - I want to live in a better world. And by “want” I don’t mean “I would like you to improve it”, but to do it by myself and motivate as many people as I can, you for example.</p>
 
@@ -190,7 +190,7 @@ const posts = [
 
 			<p class="article__text">I’ve got a conversation with “Head of integrations” at a few-year “startup X”. Below I will show and comment full content(without regarding part) of the letter, he sent me after.</p>
 
-			<p class="article__subtitle part1">This is how to not write your feedback letter.</p>
+			<h3 class="article__subtitle part1">This is how to not write your feedback letter.</h3>
 
 			<p class="article__text">Even if it is what you really think. Keep things professional.</p>
 
@@ -228,13 +228,13 @@ const posts = [
 
 			<p class="article__text">And… you did not leave, fine. Conclusion is not the only matter. Let’s read, then.</p>
 
-			<p class="article__subtitle part1">Prehistory</p>
+			<h3 class="article__subtitle part1">Prehistory</h3>
 
 			<p class="article__text">So I am a developer. I created an extension, ironically, inspired by medium.com. Part of developers articles public here or at very similar platforms. You probably faced(or facing right now if you did not pay) a lot of bars at top/bottom or even popups interrupting you while reading.</p>
 
 			<p class="article__text">I tried to remove them using the ad blocker feature — it can remove any element from the web page. But it would appear after reloading anyway. So I wrote a script that removes stuck to screen elements(like bars or popups with omnipresent cookies) permanently. And then I realized that someone, probably you, my dear reader, could find this kind of tool useful.</p>
 
-			<p class="article__subtitle part2">History</p>
+			<h3 class="article__subtitle part2">History</h3>
 
 			<p class="article__text">As you could know, to make the thing noticeable, it should be advertised. At least a little. So my plan was to expose “big” popular blockers in not doing their jobs properly. I was satisfied. But my expectations about the usefulness of these modern blockers was quite lower. The conclusion will be in the end.</p>
 
@@ -250,15 +250,15 @@ const posts = [
 
 			<p class="article__text">Few more things, I had noticed, during mine little investigation:</p>
 
-			<p class="article__subtitle part3">1. Performance and user experience.</p>
+			<h3 class="article__subtitle part3">1. Performance and user experience.</h3>
 
 			<p class="article__text">I can not call any of my computers powerful. After I enabled all extensions(average difference in weight x100 with extension of mine, in number of scripts to execute about 100 as well), every Chrome launch took much more time, then a clean version. Part of websites loads became torture. I understand tha it is not how it is supposed to work. I mean installing all the extensions at once, but I will make my message clean. These blockers apart miss a lot. Having it all together greatly increases percent of blocked popups, but noticeably reduces the performance.</p>
 
-			<p class="article__subtitle part4">2. Wrong triggering.</p>
+			<h3 class="article__subtitle part4">2. Wrong triggering.</h3>
 
 			<p class="article__text">If you don't use Google for your searches, most popup blockers trigger when you just enter a different search engine. And they will not allow you to open any result. And if you force them to, they will not allow you to open the result at the new tab by clicking ctrl + mouse click, or right click and choosing “open link in new tab”. I understand how these scripts work and why. But as a user, I actually do not want them to. It is not setting up from any settings anywhere. So far, I used my “extensions starter pack” for a couple of hours, and this “feature” annoyed me much more than any popup in this life. To remove all kinds of pollution, you have to sacrifice something clean. But if it is not removing ALL problems, and still cuts your possibilities — screw it!</p>
 
-			<p class="article__subtitle part5">Conclusion</p>
+			<h3 class="article__subtitle part5">Conclusion</h3>
 
 			<p class="article__text">So far, it is almost all I wanted to share with you about. If you stop from reading now and think about what I want to say, you, probably, would be wrong. If you are not a fastidious guy, few or probably even one popup blocker extension in pair with any fine ad blocker would be enough, to make yourself comfortable. I believe developers updating their databases and scripts full time to make you even more comfortable.</p>
 
