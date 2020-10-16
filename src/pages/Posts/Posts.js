176bba1d0c0467b5a1644145cc7c3c0a48c25dc1
unknown <romanisthere@gmail.com>
2020-10-16 13:18:20 +0300

    add filters

--- src/pages/Posts/Posts.js
+++ src/pages/Posts/Posts.js
@@ -1,4 +1,4 @@
-import { useEffect } from "preact/hooks"
+import { useEffect, useState } from "preact/hooks"
 import { Link } from 'preact-router/match'
 
 import LinkBack from '../../components/LinkBack/LinkBack'
@@ -12,10 +12,6 @@ import './Posts.css'
 const images = importAll(require.context('../../assets/posts/', false, /\.(png|jpe?g|svg)$/))
 
 const tags = [...new Set(posts.flatMap(item => item.tags))]
-console.log(tags)
-
-const postsByTags = tag =>
-    posts.filter(obj => obj.tags.includes(tag))
 
 const createItem = ({
     title,
@@ -34,21 +30,48 @@ const createItem = ({
         </Link>
     </article>
 
-const items = postsByTags('PopUpOFF').map(createItem)
+const postsByTags = tag =>
+    posts.filter(obj => obj.tags.includes(tag))
+
+
 // const allItems = postsByTags('other').map(createItem)
 const allItems = posts.map(createItem)
 
 const Posts = () => {
+    const [curTag, setTag] = useState(tags[tags.length - 1])
+    let items = postsByTags(curTag).map(createItem)
+
+    const changeTag = (newItem, e) => {
+        e.preventDefault()
+        setTag(newItem)
+        return false
+    }
+
     useEffect(() => {
         if (typeof window !== 'undefined')
             document.title = content.titles.posts
-    }, [])
+
+        items = postsByTags(curTag).map(createItem)
+    }, [curTag])
 
   	return 	<section className="main posts">
+
                 <div className="posts__wrap">
+                    <div className="posts__nav">
+                        <span className="posts__nav_item">All articles:</span>
+                    </div>
                     {allItems}
                 </div>
                 <div className="posts__wrap">
+                    <nav className="posts__nav">
+                        { tags.map((item, i) =>
+                            <a
+                                key={item}
+                                href="#"
+                                onClick={e => changeTag.call(null, item, e)}
+                                className={`posts__nav_item ${curTag === item && 'posts__nav_item-active'}`}>{item}</a>) }
+                    </nav>
+
                     {items}
                 </div>
                 <LinkBack className="posts__back" />
