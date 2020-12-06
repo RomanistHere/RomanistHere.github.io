51811e58138d739f4ff815b4feca82c5d0b7401f
romanisthere <romanisthere@gmail.com>
2020-12-06 17:32:32 +0300

    Updates

--- content.js
+++ content.js
@@ -109,7 +109,8 @@ const content = {
 		'Position': ['back', 'бэк', 'full', 'фул'],
 		'Salary': ['1000', '2000', '1500', '2500'],
 	},
-	formAction: 'http://193.46.198.230:3000/contact',
+	formAction: 'https://193.46.198.230:3000/contact',
+	// formAction: 'https://formspree.io/xdowzzgv',
 	form__typings: [
 		'Best way to fill would be like in example',
 		'You can jump to the next field by pressing tab',
